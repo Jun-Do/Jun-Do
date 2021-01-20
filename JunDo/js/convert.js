37f0ed754153830145910e322e28f9b0f68c8927
Youlac <55055760+Youlac@users.noreply.github.com>
2021-01-20 16:53:32 +0100

    Speedrun Any% Glitchness 6:18.90 Ajout conversion dollars

--- JunDo/js/convert.js
+++ JunDo/js/convert.js
@@ -1,24 +1,28 @@
 
 function updateResult(target) {
+  isEnglish = document.location.href.includes("_en")
+  symbole = isEnglish ? "$" : "€";
+  coef = isEnglish ? 0.0097 : 0.008;
+
   if(target == "input_yen") {
-    inputVal = Number(document.getElementById("input_euro").value.replace("€", ""))
-    outputVal = document.getElementById("input_yen")
+    inputVal = Number(document.getElementById("input_euro").value.replace(symbole, ""));
+    outputVal = document.getElementById("input_yen");
     if (!isNaN(inputVal)) {
-      outputVal.value = (Number(inputVal)/0.008) + " ¥"
-      console.log("Valeur convertie !")
+      outputVal.value = (Math.round((Number(inputVal)/coef)*100)/100) + " ¥";
+      console.log("Valeur convertie !");
     }
 
   } else if(target == "input_euro") {
-    inputVal = Number(document.getElementById("input_yen").value.replace("¥", ""))
-    outputVal = document.getElementById("input_euro")
+    inputVal = Number(document.getElementById("input_yen").value.replace("¥", ""));
+    outputVal = document.getElementById("input_euro");
     if (!isNaN(inputVal)) {
-      outputVal.value = (Number(inputVal)*0.008) + " €"
+      outputVal.value = (Math.round(Number(inputVal)*coef*100)/100) + " " + symbole;
     }
   
   } else {
-    console.error("Le paramètre entré ne correspond pas à celui attendu.")
+    console.error("Le paramètre entré ne correspond pas à celui attendu.");
   }
 }
 
-updateResult("input_yen")
-console.log("Cahrgement terminé.")
+updateResult("input_yen");
+console.log("Cahrgement terminé.");

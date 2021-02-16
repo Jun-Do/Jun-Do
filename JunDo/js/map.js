82c8eae4d0857c39bc0d4ce984ce3ba9bce7fb76
Youlac <55055760+Youlac@users.noreply.github.com>
2021-02-16 08:30:17 +0100

    carte amélioration

--- JunDo/js/map.js
+++ JunDo/js/map.js
@@ -1,17 +1,20 @@
 var mapGoogle = document.getElementById("map_google");
 var mapLocal = document.getElementById("map_local");
+var mapLocalInfo = document.getElementById("map_local_info");
 
 var onMapLocal = false;
 
 function ChangeMap() {
   if (onMapLocal) {
     mapLocal.style.display = "none";
+    mapLocalInfo.style.display = "none";
     mapGoogle.style.display = "block";
     onMapLocal = false;
     console.log("great");
   }
   else {
     mapLocal.style.display = "block";
+    mapLocalInfo.style.display = "block";
     mapGoogle.style.display = "none";
     onMapLocal = true;
     console.log("great");
